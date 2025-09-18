ca6ba72cf05589a7352efb76206b9377cf4e46e1
Joydip <joydeepkarmakar5480@gmail.com>
2025-09-19 01:21:18 +0530

    .

---
+++ class1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+// int main() {
+//     int a, b;
+//     scanf("%d %d",&a,&b);
+//     printf("%d %d\n",a,b);
+//     // getchar();
+//     printf("Hello, World!\n");
+//     return 0;
+// }
+int main() {
+    int a, b;
+    ()
+    return 0;
+}
